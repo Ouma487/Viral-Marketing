a17662ca3041626288798c22e3e706ac3d91a479
Paul Capgras <paul.capgras@student-cs.fr>
2022-01-28 09:04:03 +0100

    add images & comments

--- affichage.py
+++ affichage.py
@@ -1,7 +1,6 @@
 from utility import get_keys_to_list, adj_list, var
 import igraph as ig
 import matplotlib.pyplot as plt
-from extract_data import classement_influenceurs_follow
 
 
 def most_connected_nodes(vertex, edges, return_value=False):
@@ -10,6 +9,9 @@ def most_connected_nodes(vertex, edges, return_value=False):
     Args:
         vertex ([type]): [description]
         edges ([type]): [description]
+    Return:
+        liste: liste des influenceurs par ordre décroissant de de followers
+        (si return_value = True) liste: nombre de followers des influenceurs
     """
     influenceurs = []
     valeur = []
@@ -44,6 +46,17 @@ def limiter_graph(vertex, edges, N):
 
 
 def afficher_propagation(tps, nb_like, nb_personne_like, nb_personne_repost):
+    """Affiche 3 graphs qui décrivent la propagation de l'information dans le graph
+
+    Args:
+        tps (list): [description]
+        nb_like (list): [description]
+        nb_personne_like (list): [description]
+        nb_personne_repost (list): [description]
+
+    Returns:
+        [type]: [description]
+    """
     fig, axs = plt.subplots(2, 2)
     axs[0, 0].plot(tps, nb_like, 'tab:green')
     axs[0, 0].plot(tps, var(nb_like), 'tab:orange')
@@ -56,7 +69,7 @@ def afficher_propagation(tps, nb_like, nb_personne_like, nb_personne_repost):
     axs[1, 0].plot(tps, nb_personne_repost, 'tab:green')
     axs[1, 0].plot(tps, var(nb_personne_repost), 'tab:orange')
     axs[1, 0].set_title('Nombre de personne qui repost')
-    return fig, axs
+    plt.show()
 
 
 def afficher_graph(vertex, edges, nom='user', color=False):
@@ -81,4 +94,5 @@ def afficher_graph(vertex, edges, nom='user', color=False):
     else:
         g.vs['label'] = [i for i in range(n)]
 
-    return ig.plot(g)
+    ig.plot(g)
+    plt.show()
